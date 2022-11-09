dd69b42944388bd8e8fc303511bf309dc602f1cc
suwith <suwith@kookmin.ac.kr>
2022-11-09 15:56:37 +0900

    11/8

--- 17_example_2/17_example_2.ino
+++ 17_example_2/17_example_2.ino
@@ -10,19 +10,20 @@
 #define _DUTY_NEU 1476 // servo neutral position (90 degree)
 #define _DUTY_MAX 2399 // servo full counter-clockwise position (180 degree)
 
-#define LOOP_INTERVAL 50   // Loop Interval (unit: msec)
+#define LOOP_INTERVAL 50 // Loop Interval (unit: msec)
 
 Servo myservo;
-unsigned long last_loop_time;   // unit: msec
+unsigned long last_loop_time; // unit: msec
 int analogPin = 0;
 float dist;
 float dist_ema;
 float alpha = 0.5;
 void setup()
 {
-  myservo.attach(PIN_SERVO); 
+  myservo.attach(PIN_SERVO);
   myservo.writeMicroseconds(_DUTY_NEU);
-  
+  pinMode(PIN_LED, OUTPUT);
+
   Serial.begin(57600);
 }
 
@@ -36,20 +37,22 @@ void loop()
     return;
   last_loop_time += LOOP_INTERVAL;
 
-  dist = (6762.0/(a_value-9)-4.0)*10.0 - 60.0 ;
-  dist_ema = alpha*dist + (1-alpha)*dist_ema;
-  if(dist_ema <= 100.0) {
-     myservo.writeMicroseconds(_DUTY_MIN);
-     analogWrite(PIN_LED, 255);
+  dist = (6762.0 / (a_value - 9) - 4.0) * 10.0 - 60.0;
+  dist_ema = alpha * dist + (1 - alpha) * dist_ema;
+  if (dist_ema <= 100.0)
+  {
+    myservo.writeMicroseconds(_DUTY_MIN);
+    analogWrite(PIN_LED, 255);
   }
-  else if(dist_ema > 100.0 && dist_ema < 250.0){
-    double i = (dist_ema - 180)*10.25;
-    double tmp = i + 553;
-    myservo.writeMicroseconds(tmp);
+  else if (dist_ema > 100.0 && dist_ema < 250.0)
+  {
+    double i = (dist_ema - 180) * 10.25;
+    duty = i + 553;
+    myservo.writeMicroseconds(duty);
     analogWrite(PIN_LED, 0);
-    
   }
-  else {
+  else
+  {
     myservo.writeMicroseconds(_DUTY_MAX);
     analogWrite(PIN_LED, 255);
   }
@@ -59,14 +62,15 @@ void loop()
   // we need EMA filter here !!!
 
   // map distance into duty
-  
-  myservo.writeMicroseconds(duty);
 
   // print IR sensor value, distnace, duty !!!
-  Serial.print("ADC Read: "); Serial.print(a_value);
-  Serial.print(" = ");
+  Serial.print(",IR:");
+  Serial.print(a_value);
+  Serial.print(",dist:");
+  Serial.print(dist);
+  Serial.print(",ema:");
   Serial.print(dist_ema);
-  Serial.print(" Volt => Duty : ");
+  Serial.print(",servo:");
   Serial.print(duty);
-  Serial.println("usec");
+  Serial.println("");
 }
